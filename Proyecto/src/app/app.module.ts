abcf1234b936f3cd8e2fa8e926ee8efe257b8c60
Marchi14 <45884562+Marchi14@users.noreply.github.com>
2020-01-31 19:16:22 +0100

    c

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ })
   ],
   providers: [],
   bootstrap: [AppComponent]
